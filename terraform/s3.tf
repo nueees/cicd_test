994acf18055cd9f6a3e836aa1dd751e7d139931f
nueees <nueees@gmail.com>
2022-06-01 20:58:39 +0200

    Update s3.tf

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -7,3 +7,10 @@ resource "aws_s3_bucket" "tap_jira_states" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "tap_jira_states" {
+  bucket                  = aws_s3_bucket.tap_jira_states.id
+  block_public_acls       = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+  ignore_public_acls      = true
+}
