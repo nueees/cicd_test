cf3d9a00e1c05aa404a830dc7987d4e796e2005e
nueees <nueees@gmail.com>
2022-06-02 07:48:44 +0200

    Update s3.tf

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,16 +1,25 @@
-resource "aws_s3_bucket" "tap_jira_states" {
-  bucket = "secho-gcs-jira-metrics-state"
-  acl    = "private"
+terraform {
+  required_version = ">= 1.0"
   
-  versioning {
-    enabled = true
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.48.0"
+    }
+  }
+  
+  backend "s3" {
+  
+    bucket      = "secho-gcs-jira-metrics-state"
+    key         = "SECHO-2286/tfstate"
+    region      = "eu-central-1"
+    dynamodb_table = "secho-jira-metrics"
+    profile     = "secho"
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "tap_jira_states" {
-  bucket                  = aws_s3_bucket.tap_jira_states.id
-  block_public_acls       = true
-  block_public_policy     = true
-  restrict_public_buckets = true
-  ignore_public_acls      = true
+provider "aws" {
+  region = "eu-central-1"
+  profile = "secho-tools"
+  allowed_account_ids = ["623160492249"]
 }
