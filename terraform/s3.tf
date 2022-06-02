d3a70e6b73e99d73caafc9cc98580d92e80f9481
nueees <nueees@gmail.com>
2022-06-02 08:03:25 +0200

    Update s3.tf

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,3 +1,22 @@
+terraform {
+  required_version = ">= 1.0"
+  
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.48.0"
+    }
+  }
+  
+  backend "s3" {
+  
+    bucket      = "secho-gcs-jira-metrics-state"
+    key         = "SECHO-2286/tfstate"
+    region      = "eu-central-1"
+    dynamodb_table = "secho-jira-metrics"
+  }
+}
+
 provider "aws" {
   region = "eu-central-1"
   profile = "secho-tools"
