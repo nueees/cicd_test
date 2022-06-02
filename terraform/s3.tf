22fe2e029cf94ef7580d3778e8f078fcdeec3877
nueees <nueees@gmail.com>
2022-06-02 08:10:25 +0200

    Update s3.tf

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -13,7 +13,7 @@ terraform {
     bucket      = "secho-gcs-jira-metrics-state"
     key         = "SECHO-2286/tfstate"
     region      = "eu-central-1"
-    dynamodb_table = "secho-jira-metrics"
+    dynamodb_table = "secho-jira-metrics-tf-lock"
   }
 }
 
@@ -23,10 +23,23 @@ provider "aws" {
   allowed_account_ids = ["623160492249"]
 }
 
+# S3 bucket for backend
 resource "aws_s3_bucket" "tap_jira_states" {
   bucket = "secho-gcs-jira-metrics-state"
-  acl    = "private"
-  tags = {
-    Name        = "test-terraform-jira-states"
+
+  versioning {
+    enabled = true # Prevent from deleting tfstate file
+  }
+}
+
+# DynamoDB for terraform state lock
+resource "aws_dynamodb_table" "tap_jira_state_lock" {
+  name           = "secho-jira-metrics-tf-lock"
+  hash_key       = "LockID"
+  billing_mode   = "PAY_PER_REQUEST"
+
+  attribute {
+    name = "LockID"
+    type = "S"
   }
 }
