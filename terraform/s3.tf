e3a34982293cffacfd4b56dc3dd46f029cee8b03
nueees <nueees@gmail.com>
2022-06-02 10:05:38 +0200

    Update s3.tf

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -6,15 +6,3 @@ resource "aws_s3_bucket" "tap_jira_states" {
     enabled = true # Prevent from deleting tfstate file
   }
 }
-
-# DynamoDB for terraform state lock
-resource "aws_dynamodb_table" "tap_jira_state_lock" {
-  name           = "secho-jira-metrics-tf-lock"
-  hash_key       = "LockID"
-  billing_mode   = "PAY_PER_REQUEST"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
